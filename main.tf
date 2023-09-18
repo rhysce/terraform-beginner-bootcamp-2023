f87ad62bd61ed4bfdab7f347481b0d0845e18529
Rhys Clement-Evans <rhys@clement-evans.co.uk>
2023-09-18 18:15:36 +0000

    Added comment to shut down gitpod after changes.

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 # Type 'git pull' in terminal window below to refresh from Github.
 # Note to self - CTRL-S to save changes and trigger gitpod
 # Once change saved, switch to Source Control view and commit.
+# Finally, when finished, be sure to end the gitpod instance
